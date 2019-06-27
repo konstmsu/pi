0709bb2f7aef37e8d3333a51d73ffd66f58398ba
Konstantin Spirin <konstmsu@gmail.com>
2019-06-27 14:48:03 +0000

    Gracefully handle Ctrl+C

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
-#include <stdio.h>
+#include <signal.h>
 #include <wiringPi.h>
+
 using namespace std;
+
+static volatile sig_atomic_t sigint_received = false;
 
 int main() {
   cout << "Started!" << endl;
+  signal(SIGINT, [](int) { sigint_received = true; });
 
   if (wiringPiSetup() < 0) {
-    cout << "setup wiring pi failed" << endl;
+    cout << "Wiring PI setup failed" << endl;
     return 1;
   }
 
-  int c;
-  int ledPin = 29;
+  int ledPin = 8;
   pinMode(ledPin, OUTPUT);
 
-  for (int i = 0; i < 10; i++) {
-    cout << "Blinking" << i << endl;
-    digitalWrite(ledPin, HIGH);
-    delay(100);
+  for (int i = 0; i < 100; i++) {
+    if (sigint_received)
+      break;
+
+    cout << "Blinking " << i << endl;
     digitalWrite(ledPin, LOW);
+    delay(50);
+    digitalWrite(ledPin, HIGH);
     delay(100);
   }
 
