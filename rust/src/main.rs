21930b38d77e416c10734cbb9c579f995ae0f5b6
Konstantin Spirin <konstmsu@gmail.com>
2019-06-27 17:01:37 +0000

    Added 2 more LEDs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,14 +3,15 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let led = LED::new(2);
+    let leds = [LED::new(2), LED::new(3), LED::new(4)];
 
-    for n in 1..21 {
-        println!("Blinking {}", n);
+    for i in 0..20 {
+        println!("Blinking {}", i);
+        let led = &leds[i % leds.len()];
         led.on();
-        thread::sleep(Duration::from_millis(500));
-        led.off();
         thread::sleep(Duration::from_millis(300));
+        led.off();
+        thread::sleep(Duration::from_millis(200));
     }
 
     println!("Finished")
